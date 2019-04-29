0c7d55b5a0c8ad333588a81ff7ffb5f65a0fb932
mahao <mahao>
2019-04-29 19:10:18 +0800

    创建表结构

---
+++ src/main/resources/createTable.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.26 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `profitdetail` (
+	`code` char (30),
+	`pcode` char (60),
+	`creattime` datetime ,
+	`price` Decimal (22),
+	`usercode` char (30)
+); 
